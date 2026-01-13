1a7d44d2c292a55ac89272a912ba4caefeee8fd1
Eric de Quartel <edequartel@protonmail.com>
2026-01-13 07:15:34 +0100

    update

---
+++ supabase-config.js
@@ -0,0 +1,4 @@
+export const supabaseConfig = {
+  url: "https://zrcdyzcfsdlmqqwdhctk.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpyY2R5emNmc2RsbXFxd2RoY3RrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjgxOTgyNzUsImV4cCI6MjA4Mzc3NDI3NX0.voT1eh_FbBkrv7ZMN7B8VRRbrab7tyx3eV6JuXy4ySs"
+};
